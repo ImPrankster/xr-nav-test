1b9207547d83118a7fe9528fd02bee0ab0c57d86
Ruofan Wang <ruofanwang1@lenovo.com>
2025-09-14 17:55:12 +0800

    added some simple components

--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -1,17 +1,59 @@
+import { Physics, usePlane } from "@react-three/cannon";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { XR, type XRStore } from "@react-three/xr";
+import {
+	noEvents,
+	PointerEvents,
+	XR,
+	XROrigin,
+	type XRStore,
+} from "@react-three/xr";
+import { Suspense } from "react";
+import type { PhysicalBodyCommonProps } from "~/helpers/types";
+import { Chair, Lamp, Mug, Table } from "./furniture";
 
 export default function Scene({ xrStore }: { xrStore: XRStore }) {
 	return (
-		<Canvas shadows camera={{ position: [5, 2, 10], fov: 50 }}>
+		<Canvas
+			onPointerMissed={() => console.log("missed")}
+			dpr={[1, 2]}
+			shadows
+			events={noEvents}
+			camera={{ position: [-40, 40, 40], fov: 25 }}
+		>
+			<PointerEvents />
+			<OrbitControls />
 			<XR store={xrStore}>
-				<ambientLight intensity={0.5} />
-				<pointLight position={[2, 2, 2]} intensity={1} />
-				<mesh>
-					<boxGeometry args={[1, 1, 1]} />
-					<meshStandardMaterial color="#ff69b4" />
-				</mesh>
+				<color attach="background" args={["#171720"]} />
+				<ambientLight intensity={0.2} />
+				<pointLight position={[-20, -5, -20]} color="red" />
+				<Suspense>
+					<Physics allowSleep={false} iterations={15} gravity={[0, -50, 0]}>
+						<Floor position={[0, -5, 0]} />
+						<Chair position={[0, 0, -2.52]} />
+						<Table position={[8, -0.5, 0]} />
+						<Mug position={[8, 3, 0]} />
+						<Lamp position={[0, 15, 0]} />
+					</Physics>
+				</Suspense>
+				<group position={[0, -5, 0]}>
+					<XROrigin />
+				</group>
 			</XR>
 		</Canvas>
+	);
+}
+
+function Floor(props: PhysicalBodyCommonProps) {
+	const [ref] = usePlane(() => ({
+		type: "Static",
+		rotation: [-Math.PI / 2, 0, 0],
+		...props,
+	}));
+	return (
+		<mesh ref={ref} receiveShadow>
+			<planeGeometry args={[100, 100]} />
+			<meshPhongMaterial color="#878790" />
+		</mesh>
 	);
 }
